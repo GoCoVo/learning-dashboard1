ddf22bee7ad442db8fa499988219154d3671c15e
GoCoVo <coriellism@gmail.com>
2024-11-22 10:27:15 -0500

    Initial setup

---
+++ src/components/ui/App.js
@@ -0,0 +1,9 @@
+{
+    "name": "learning-dashboard",
+    "version": "1.0.0",
+    "dependencies": {
+      "react": "^18.2.0",
+      "recharts": "^2.12.0",
+      "@radix-ui/react-tabs": "^1.0.0"
+    }
+  }
